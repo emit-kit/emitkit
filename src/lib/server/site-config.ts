2322e1a02fe96cb9b3a907a3edb80e7fafe7b278
Jaeden <ch.schwartze@gmail.com>
2025-12-03 09:11:53 +0100

    chore: update vercel url

--- src/lib/server/site-config.ts
+++ src/lib/server/site-config.ts
@@ -1,10 +1,10 @@
-import { VERCEL_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 // VERCEL_URL is just the domain without protocol (e.g., "localhost:5173" or "my-app.vercel.app")
 const getAppUrl = () => {
 	const protocol = import.meta.env.PROD ? 'https://' : 'http://';
 
-	return `${protocol}${VERCEL_URL}`;
+	return `${protocol}${env.VERCEL_PROJECT_PRODUCTION_URL ?? env.VERCEL_URL}`;
 };
 
 export const siteConfig = {
