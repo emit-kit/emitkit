2fce9d9a0b98e0051a76672f7aede853d244d847
Jaeden <ch.schwartze@gmail.com>
2025-12-05 15:46:31 +0100

    fix: icon url

--- src/routes/(app)/events/[folder_id]/[channel_id]/+page.ts
+++ src/routes/(app)/events/[folder_id]/[channel_id]/+page.ts
@@ -9,8 +9,7 @@ export const load: PageLoad = async ({ params, parent }) => {
 				title: parentData.folderName,
 				url: `/events/${params.folder_id}/${params.channel_id}`,
 				metadata: {
-					// TODO, if this folder has a site url, we get the favicon
-					// iconUrl: parentData.folderIconUrl
+					iconUrl: parentData.folderIconUrl
 				}
 			}
 		]
